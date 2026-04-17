d95f0ec25d14efcf67327660898d5feae0717a85
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-17 23:56:14 +0500

    Auth activity to be designded and implement

--- app/src/main/java/com/example/logicmed/AuthenticationScreen.java
+++ app/src/main/java/com/example/logicmed/AuthenticationScreen.java
@@ -3,12 +3,20 @@ package com.example.logicmed;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AuthenticationScreen extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private TabLayoutMediator tabLayoutMediator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,32 @@ public class AuthenticationScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+    }
+
+    private void init() {
+        tabLayout = findViewById(R.id.auth_tab_layout);
+        viewPager2 = findViewById(R.id.auth_viewpager2);
+        viewPager2.setAdapter(
+                new AuthViewPagerAdapter(this)
+        );
+        tabLayoutMediator = new TabLayoutMediator(
+                tabLayout,
+                viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int i) {
+                        switch (i) {
+                            case 0:
+                                tab.setText("Login");
+                                break;
+                            case 1:
+                                tab.setText("Signup");
+                                break;
+                        }
+                    }
+                }
+        );
+
     }
 }
