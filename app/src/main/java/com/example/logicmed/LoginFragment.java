12ba394f96c19d444506bb5d6bdf0d08b7b5c19e
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-21 21:54:14 +0500

    Login Frag AddeD

--- app/src/main/java/com/example/logicmed/LoginFragment.java
+++ app/src/main/java/com/example/logicmed/LoginFragment.java
@@ -1,64 +1,104 @@
 package com.example.logicmed;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 public class LoginFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextInputEditText teEmail;
+    TextInputEditText tePassword;
+    MaterialButton btnLogin;
+    ProgressBar progressBar;
+    private FirebaseAuth firebaseAuth;
 
     public LoginFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+
+        btnLogin.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(teEmail.getText()).toString();
+            String password = Objects.requireNonNull(tePassword.getText()).toString();
+            firebaseAuthLogin(email, password);
+        });
+
+    }
+    private void init(View view) {
+        teEmail = view.findViewById(R.id.login_email);
+        tePassword = view.findViewById(R.id.login_pass);
+        btnLogin = view.findViewById(R.id.login_btn);
+        progressBar = view.findViewById(R.id.login_progress_bar);
+        progressBar.setVisibility(View.GONE);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+    private void firebaseAuthLogin(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        Context context = requireContext();
+        Activity activity = requireActivity();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if (
+                                    context.getSharedPreferences(KeyUtils.userPrefFileKey, Context.MODE_PRIVATE)
+                                            .edit()
+                                            .putBoolean(KeyUtils.isLoggedInPrefKey, true)
+                                            .commit()
+                            ) {
+                                startActivity(
+                                        new Intent(
+                                                context,
+                                                MainActivity.class
+                                        )
+                                );
+                                activity.finish();
+                            }
+                            else {
+                                Toast.makeText(context, "Unable to Update Login State", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else {
+                            String error = task.getException() != null ? task.getException().getMessage() : "Unable to Login";
+                            Toast.makeText(context, error, Toast.LENGTH_LONG).show();
+                        }
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
