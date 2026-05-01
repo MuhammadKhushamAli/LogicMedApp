7c7d41a1bfc8dece2fd0d3a49a8d0d7a9a219eed
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-05-01 11:44:40 +0500

    chat frag addeD

---
+++ app/src/main/java/com/example/logicmed/ChatFragment.java
@@ -0,0 +1,26 @@
+package com.example.logicmed;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
